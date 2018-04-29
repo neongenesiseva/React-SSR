a15eaff864f67b59f02c18846d8daa0aa96fa56e
XIE <33032674+JuneFan@users.noreply.github.com>
2018-04-28 23:54:10 -0400

    add comment (#1)

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './components/Home';
+//Try Branch
 // conflicts
 //use hydrate when we imported the HTML string in the renderer.js ${content}
 ReactDOM.hydrate(<Home />,document.getElementById("app"))
