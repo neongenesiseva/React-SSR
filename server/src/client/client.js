d82f457869c0277bd8eeaa3943aae7b48fb0881a
David Li <ruoyu1993@gmail.com>
2018-04-28 23:38:54 -0400

    for conflicting

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './components/Home';
-
+// conflicts
 //use hydrate when we imported the HTML string in the renderer.js ${content}
 ReactDOM.hydrate(<Home />,document.getElementById("app"))
